36c6916ba6d6d7cf32d3d9b2c49a7577489dc5f4
Amit Singh <ajamit24singh@gmail.com>
2025-06-23 22:06:08 -0500

    Force redeploy to establish database connection

--- config/database.js
+++ config/database.js
@@ -1,14 +1,19 @@
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
-    connection: {
-      host: env('PGHOST', env('DATABASE_HOST', '127.0.0.1')),
-      port: env.int('PGPORT', env.int('DATABASE_PORT', 5432)),
-      database: env('PGDATABASE', env('DATABASE_NAME', 'strapi')),
-      user: env('PGUSER', env('DATABASE_USERNAME', 'strapi')),
-      password: env('PGPASSWORD', env('DATABASE_PASSWORD', 'password')),
+    connection: env('DATABASE_URL') ? {
+      connectionString: env('DATABASE_URL'),
       ssl: env.bool('DATABASE_SSL', false) && {
-        rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
+        rejectUnauthorized: false,
+      },
+    } : {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'password'),
+      ssl: env.bool('DATABASE_SSL', false) && {
+        rejectUnauthorized: false,
       },
     },
     debug: false,
